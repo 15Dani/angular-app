bccb67eb7c93e567aa19a2b3dd0478e5c85413cc
Danielia Oliveira <danielia.de@bol.com.br>
2020-05-18 11:49:01 -0300

    Correções

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,35 +10,35 @@ export class AppComponent {
   title = 'Testeauditivo';
 
 
-msbapTitle = 'Audio Title';
-msbapAudioUrl = 'Link to audio URL';
+// msbapTitle = 'Audio Title';
+// msbapAudioUrl = 'Link to audio URL';
 
-msbapDisplayTitle = false;
-msbapDisplayVolumeControls = true;
+// msbapDisplayTitle = false;
+// msbapDisplayVolumeControls = true;
 
-msaapDisplayTitle = true;
-msaapDisplayPlayList = true;
-msaapPageSizeOptions = [2,4,6];
+// msaapDisplayTitle = true;
+// msaapDisplayPlayList = true;
+// msaapPageSizeOptions = [2,4,6];
 
-msaapDisplayVolumeControls = true;
+// msaapDisplayVolumeControls = true;
 
-// Material Style Advance Audio Player Playlist
-playlist: Track[] = [
-  {
-    title: 'Audio One Title',
-    link: '/../assets/audios/audios4/a1.mp3'
-  },
+// // Material Style Advance Audio Player Playlist
+// playlist: Track[] = [
+//   {
+//     title: 'Audio One Title',
+//     link: '/../assets/audios/audios4/a1.mp3'
+//   },
 
   // {
-  //   title: 'Audio Three Title',
-  //   link: '/Testeauditivo/audios4/audios4/c1.mp3'
-  // },
+  //  title: 'Audio Three Title',
+  //  link: '/Testeauditivo/audios4/audios4/c1.mp3'
+  //  },
   // {
   //   title: 'Audio Two Title',
   //   link: '/Testeauditivo/audios4/audios4/o1.mp3'
   // },
 
-];
+// ];
 
 
 }
